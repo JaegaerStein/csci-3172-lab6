c8a0ca570bc4138081554a2a942911f3050778dc
steinhauer <jg225260@dal.ca>
2025-03-12 13:36:45 -0300

    Working site with pages and components

--- labs/lab6/src/App.js
+++ labs/lab6/src/App.js
@@ -1,68 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const Home = () => (
-  <div className="container mt-5">
-    <h1>Welcome to My Portfolio</h1>
-    <p>This is the home page of my portfolio.</p>
-  </div>
-);
-
-const About = () => (
-  <div className="container mt-5">
-    <h1>About Me</h1>
-    <p>Education: Bachelor’s in Computer Science</p>
-    <p>Experience: Front-End Developer Intern</p>
-    <p>Skills: React, JavaScript, HTML, CSS, Bootstrap</p>
-  </div>
-);
-
-const Projects = () => (
-  <div className="container mt-5">
-    <h1>Projects</h1>
-    <p>Coming soon...</p>
-  </div>
-);
-
-const NotFound = () => (
-  <div className="container mt-5">
-    <h1>404 - Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-  </div>
-);
-
-const Header = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <div className="container">
-      <Link className="navbar-brand" to="/">Portfolio</Link>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav me-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/projects">Projects</Link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-);
-
-const Footer = () => (
-  <footer className="bg-light py-3 mt-5">
-    <div className="container text-center">
-      <p>&copy; 2025 My Portfolio</p>
-    </div>
-  </footer>
-);
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  return (
+  return ( 
     <Router>
       <Header />
       <Routes>
---
+++ labs/lab6/src/pages/Projects.js
@@ -0,0 +1,16 @@
+const Projects = () => {
+    return (
+      <div className="container mt-4">
+        <h1>Projects</h1>
+        <p>Here are some of my projects.</p>
+        <ul>
+          <li>Created a fully normalized NBA Statistics Database using MySQL</li>
+          <li><a href="https://jaegaerstein.github.io/JaegarStein.github.io/index.html" target="_blank">photography website</a> (HTML, CSS, JS)</li>
+          <li>Collaborated on an Uber-like Android app development project using React Native and Spring Boot.</li>
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Projects;
+  
